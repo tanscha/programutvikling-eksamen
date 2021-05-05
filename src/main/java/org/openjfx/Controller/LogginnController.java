50ce36682f1828b08ebf2217fa6f03f796c7dd67
Toril <torilsusanne@hotmail.com>
2021-05-05 22:10:35 +0200

    fxml

--- src/main/java/org/openjfx/Controller/LogginnController.java
+++ src/main/java/org/openjfx/Controller/LogginnController.java
@@ -9,12 +9,11 @@ import org.openjfx.App;
 
 import java.io.IOException;
 
-public class AdminLogginnController {
+public class LogginnController {
 
-
-    public Button btnTilbake;
     public Button btnLoggInn;
     public Button btnUten;
+
     @FXML
     private PasswordField txtPassord;
     @FXML
@@ -22,33 +21,23 @@ public class AdminLogginnController {
     @FXML
     private Label lblFeil;
 
-    //Metode som sjekker om brukernavn og passord er riktig
-    //Passord er case-sensitiv, det er ikke brukernavnet
-    private void innlogging() throws IOException {
+
+    private void logginn() throws IOException {
         if (!txtPassord.getText().equals("passord") || !txtBrukernavn.getText().toLowerCase().equals("admin")) {
             lblFeil.setText("Feil brukernavn og/eller passord!");
             txtPassord.clear();
         } else {
-            App.setRoot("adminkomponenter");
+            App.setRoot("produkter");
         }
     }
 
-    //tilbake knapp
-    @FXML
-    void btnTilbake(ActionEvent event) throws IOException {
-        App.setRoot("hovedside");
-    }
-
-    //logg inn knapp
     @FXML
     void btnLoggInn(ActionEvent event) throws IOException {
-        innlogging();
-
+        logginn();
     }
 
-    //Knapp som gjøre det lettere å gå frem og tilbake uten alltid å måtte logge inn (under vurdering og programmering)
     @FXML
     void loggInnUten(ActionEvent event) throws IOException {
-        App.setRoot("adminkomponenter");
+        App.setRoot("produkter");
     }
 }
