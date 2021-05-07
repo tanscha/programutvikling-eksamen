5e527b6ca8727b75658dedfa77a5ecfc01d7a9b8
Tanja Aakerholt <tanja.aakerholt@hotmail.com>
2021-05-07 12:46:31 +0200

    nyBruker.fxml

--- src/main/java/org/openjfx/Bruker/BrukerRegister.java
+++ src/main/java/org/openjfx/Bruker/BrukerRegister.java
@@ -4,6 +4,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class BrukerRegister {
     private static Bruker aktivBruker;
@@ -23,4 +25,35 @@ public class BrukerRegister {
      public static void oppdater(){
          brukerObservableList.setAll(brukerArrayList);
      }
+
+    public static ArrayList<Bruker> getBrukerArrayList() {
+        return brukerArrayList;
+    }
+
+    public static void setBrukerArrayList(ArrayList<Bruker> brukerArrayList) {
+        BrukerRegister.brukerArrayList = brukerArrayList;
+        oppdater();
+    }
+
+    public static ObservableList<Bruker> getBrukerObservableList() {
+        return brukerObservableList;
+    }
+
+    public static void setBrukerObservableList(ObservableList<Bruker> brukerObservableList) {
+        BrukerRegister.brukerObservableList = brukerObservableList;
+    }
+
+    public static ObservableList<Bruker> getAktivBrukerListe() {
+        return aktivBrukerListe;
+    }
+
+    public static void setAktivBrukerListe(ObservableList<Bruker> aktivBrukerListe) {
+        BrukerRegister.aktivBrukerListe = aktivBrukerListe;
+    }
+    public static void StreamBrukernavn(String s){
+         BrukerRegister.setAktivBrukerListe
+                 (BrukerRegister.getBrukerObservableList().stream().filter(x ->
+                         x.getBrukernavn().toLowerCase().contains(s.toLowerCase())).
+                         collect(Collectors.toCollection(FXCollections::observableArrayList)));
+    }
 }
